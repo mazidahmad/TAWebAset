33aaff894c35ef47e25713fcb6a6de533f087ef3
rizkyahf <rizky.agung.tif17@polban.ac.id>
2019-05-15 10:03:04 +0700

    edit navbar

---
+++ link/sidebar-maintain.php
@@ -0,0 +1,41 @@
+<div class="sidebar" data-color="orange">
+      <!--
+        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+    -->
+      <div class="logo">
+        <!-- <a href="http://www.creative-tim.com" class="simple-text logo-mini">
+          CT
+        </a> -->
+        <a class="simple-text logo-normal">
+          SIPAT DISPERKIM
+        </a>
+      </div>
+      <div class="sidebar-wrapper" id="sidebar-wrapper">
+        <ul class="nav">
+          <li class="active ">
+            <a href="./dashboard.html" class="d-flex align-items-center">
+              <i class="now-ui-icons design_app"></i>
+              <h5 class="font-weight-bold mb-0">Dashboard</h5>
+            </a>
+          </li>
+          <li>
+            <a href="./icons.html" class="d-flex align-items-center">
+              <i class="now-ui-icons education_atom"></i>
+              <h5 class="font-weight-bold mb-0">Daftar Kebutuhan Pemeliharaan Barang Milik Daerah</h5>
+            </a>
+          </li>
+          <li>
+            <a href="./map.html" class="d-flex align-items-center">
+              <i class="now-ui-icons location_map-big"></i>
+              <h5 class="font-weight-bold mb-0">Jadwal Pemeliharaan</h5>
+            </a>
+          </li>
+          <li>
+            <a href="./notifications.html" class="d-flex align-items-center">
+              <i class="now-ui-icons ui-1_bell-53"></i>
+              <h5 class="font-weight-bold mb-0">Kartu Pemeliharaan Barang</h5>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
--- link/sidebar.php
+++ link/sidebar.php
@@ -13,28 +13,45 @@
       <div class="sidebar-wrapper" id="sidebar-wrapper">
         <ul class="nav">
           <li class="active ">
-            <a href="./dashboard.html" class="d-flex align-items-center">
+            <a href="index.php" class="d-flex align-items-center">
               <i class="now-ui-icons design_app"></i>
               <h5 class="font-weight-bold mb-0">Dashboard</h5>
             </a>
           </li>
+          
           <li>
-            <a href="./icons.html" class="d-flex align-items-center">
-              <i class="now-ui-icons education_atom"></i>
-              <h5 class="font-weight-bold mb-0">Daftar Kebutuhan Pemeliharaan Barang Milik Daerah</h5>
-            </a>
-          </li>
-          <li>
-            <a href="./map.html" class="d-flex align-items-center">
-              <i class="now-ui-icons location_map-big"></i>
-              <h5 class="font-weight-bold mb-0">Jadwal Pemeliharaan</h5>
-            </a>
-          </li>
-          <li>
-            <a href="./notifications.html" class="d-flex align-items-center">
+            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle d-flex align-items-center">
               <i class="now-ui-icons ui-1_bell-53"></i>
-              <h5 class="font-weight-bold mb-0">Kartu Pemeliharaan Barang</h5>
+              <h5 class="font-weight-bold mb-0">Inventarisasi Aset</h5>
             </a>
+            <ul class="collapse list-unstyled" id="pageSubmenu">
+              <li>
+                  <a href="#">Page 1</a>
+              </li>
+              <li>
+                  <a href="#">Page 2</a>
+              </li>
+              <li>
+                  <a href="#">Page 3</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#pageSubmenu2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle d-flex align-items-center">
+              <i class="now-ui-icons ui-1_bell-53"></i>
+              <h5 class="font-weight-bold mb-0">Pemeliharaan Aset</h5>
+            </a>
+            <ul class="collapse list-unstyled" id="pageSubmenu2">
+              <li>
+                  <a href="#">Page 1</a>
+              </li>
+              <li>
+                  <a href="#">Page 2</a>
+              </li>
+              <li>
+                  <a href="#">Page 3</a>
+              </li>
+            </ul>
           </li>
         </ul>
       </div>
