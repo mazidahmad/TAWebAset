933e5a6619ccf73ddc13cc4d9096df6d3e0d57b5
Regawa <regawa04@gmail.com>
2019-05-22 18:23:09 +0700

    USULAN PENGHAPUSAN Complete
    insert-delete-update done

--- link/_usulan-penghapusan-edit.php
+++ link/_usulan-penghapusan-edit.php
@@ -1,110 +1,105 @@
-<div class="panel-header panel-header-lg">
-    <img src="assets/img/header/bg-inventory.jpg" alt="bg-inventory" width="100%">
-</div>
-<div class="content">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <p class="card-title text-center"><strong>Input DAFTAR USULAN BARANG YANG AKAN DIHAPUS</p>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-12">
+<?php
+include 'connect.php';
+$ID_USULAN = $_GET['id'];
+$query = "SELECT * FROM USULAN_PENGHAPUSAN WHERE ID_USULAN='$ID_USULAN'";
+$result = mysqli_query($connect, $query);
+if ($row = mysqli_fetch_array($result)) {
+    ?>
+    <div class="panel-header panel-header-lg">
+        <img src="assets/img/header/bg-inventory.jpg" alt="bg-inventory" width="100%">
+    </div>
+    <div class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <p class="card-title text-center"><strong>Update DAFTAR USULAN BARANG YANG AKAN DIHAPUS</p>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-12">
 
-                            <form method="post" action="link/crud.php?process=insert_usulan"
-                                enctype="multipart/form-data">
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="formGroupExampleInput">NOMOR_KODE_BARANG :</label>
-                                        <input type="text" class="form-control" id="formGroupExampleInput"
-                                            name="NOMOR_KODE_BARANG">
+                                <form method="post" action="link/crud.php?process=update_usulan" enctype="multipart/form-data">
+                                    <input type="hidden" name="id" <?php echo "value='$row[ID_USULAN]'"; ?>>
+                                    <div class="form-row">
+                                        <div class="form-group col-md-6">
+                                            <label for="formGroupExampleInput">NOMOR_KODE_BARANG :</label>
+                                            <input type="text" class="form-control" id="formGroupExampleInput" name="NOMOR_KODE_BARANG" <?php echo "value='$row[NOMOR_KODE_BARANG]'"; ?>>
+                                        </div>
+                                        <div class="form-group col-md-6">
+                                            <label for="formGroupExampleInput">NOMOR_REGISTER :</label>
+                                            <input type="text" class="form-control" id="formGroupExampleInput" name="NOMOR_REGISTER" <?php echo "value='$row[NOMOR_REGISTER]'"; ?>>
+                                        </div>
                                     </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="formGroupExampleInput">NOMOR_REGISTER :</label>
-                                        <input type="text" class="form-control" id="formGroupExampleInput"
-                                            name="NOMOR_REGISTER">
+                                    <div class="form-row">
+                                        <div class="form-group col-md-6">
+                                            <label for="formGroupExampleInput">NAMA_BARANG :</label>
+                                            <input type="text" class="form-control" id="formGroupExampleInput" name="NAMA_BARANG" <?php echo "value='$row[NAMA_BARANG]'"; ?>>
+                                        </div>
+                                        <div class="form-group col-md-6">
+                                            <label for="formGroupExampleInput">MERK_TIPE:</label>
+                                            <input type="text" class="form-control" id="formGroupExampleInput" name="MERK_TIPE" <?php echo "value='$row[MERK_TIPE]'"; ?>>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="formGroupExampleInput">NAMA_BARANG :</label>
-                                        <input type="text" class="form-control" id="formGroupExampleInput"
-                                            name="NAMA_BARANG">
+                                    <div class="form-row">
+                                        <div class="form-group col-md-6">
+                                            <label for="formGroupExampleInput">NO_SERTIFIKAT:</label>
+                                            <input type="text" class="form-control" id="formGroupExampleInput" name="NO_SERTIFIKAT" <?php echo "value='$row[NO_SERTIFIKAT]'"; ?>>
+                                        </div>
+                                        <div class="form-group col-md-6">
+                                            <label for="formGroupExampleInput">BAHAN :</label>
+                                            <input type="text" class="form-control" id="formGroupExampleInput" name="BAHAN" <?php echo "value='$row[BAHAN]'"; ?>>
+                                        </div>
                                     </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="formGroupExampleInput">MERK_TIPE:</label>
-                                        <input type="text" class="form-control" id="formGroupExampleInput"
-                                            name="MERK_TIPE">
+                                    <div class="form-row">
+                                        <div class="form-group col-md-6">
+                                            <label for="formGroupExampleInput">CARA_PEROLEHAN :</label>
+                                            <input type="text" class="form-control" id="formGroupExampleInput" name="CARA_PEROLEHAN" <?php echo "value='$row[CARA_PEROLEHAN]'"; ?>>
+                                        </div>
+                                        <div class="form-group col-md-6">
+                                            <label for="formGroupExampleInput">TAHUN_PEROLEHAN:</label>
+                                            <input type="text" class="form-control" id="formGroupExampleInput" name="TAHUN_PEROLEHAN" <?php echo "value='$row[TAHUN_PEROLEHAN]'"; ?>>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="formGroupExampleInput">NO_SERTIFIKAT:</label>
-                                        <input type="text" class="form-control" id="formGroupExampleInput"
-                                            name="NO_SERTIFIKAT">
+                                    <div class="form-row">
+                                        <div class="form-group col-md-6">
+                                            <label for="formGroupExampleInput">UKURAN_BARANG :</label>
+                                            <input type="text" class="form-control" id="formGroupExampleInput" name="UKURAN_BARANG" <?php echo "value='$row[UKURAN_BARANG]'"; ?>>
+                                        </div>
+                                        <div class="form-group col-md-6">
+                                            <label for="formGroupExampleInput">KEADAAN_BARANG :</label>
+                                            <input type="text" class="form-control" id="formGroupExampleInput" name="KEADAAN_BARANG" <?php echo "value='$row[KEADAAN_BARANG]'"; ?>>
+                                        </div>
                                     </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="formGroupExampleInput">BAHAN :</label>
-                                        <input type="text" class="form-control" id="formGroupExampleInput" name="BAHAN">
+                                    <div class="form-row">
+                                        <div class="form-group col-md-6">
+                                            <label for="formGroupExampleInput">JUMLAH_BARANG:</label>
+                                            <input type="text" class="form-control" id="formGroupExampleInput" name="JUMLAH_BARANG" <?php echo "value='$row[JUMLAH_BARANG]'"; ?>>
+                                        </div>
+                                        <div class="form-group col-md-6">
+                                            <label for="formGroupExampleInput">JUMLAH_HARGA :</label>
+                                            <input type="text" class="form-control" id="formGroupExampleInput" name="JUMLAH_HARGA" <?php echo "value='$row[JUMLAH_HARGA]'"; ?>>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="formGroupExampleInput">CARA_PEROLEHAN :</label>
-                                        <input type="text" class="form-control" id="formGroupExampleInput"
-                                            name="CARA_PEROLEHAN">
+                                    <div class="form-row">
+                                        <div class="form-group col-md-6">
+                                            <label for="formGroupExampleInput">KETERANGAN :</label>
+                                            <input type="text" class="form-control" id="formGroupExampleInput" name="KETERANGAN" <?php echo "value='$row[KETERANGAN]'"; ?>>
+                                        </div>
                                     </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="formGroupExampleInput">TAHUN_PEROLEHAN:</label>
-                                        <input type="text" class="form-control" id="formGroupExampleInput"
-                                            name="TAHUN_PEROLEHAN">
+                                    <div class="form-row">
+                                        <div class="form-group col-md-6 align-middle">
+                                            <button type="submit" name="update_usulan" class="btn btn-primary">Update</button>
+                                            <button value="kembali" class="btn btn-secondary"><a href="../index.php?page=usulan-penghapusan.php">Back</a></button>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="formGroupExampleInput">UKURAN_BARANG :</label>
-                                        <input type="text" class="form-control" id="formGroupExampleInput"
-                                            name="UKURAN_BARANG">
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="formGroupExampleInput">KEADAAN_BARANG :</label>
-                                        <input type="text" class="form-control" id="formGroupExampleInput"
-                                            name="KEADAAN_BARANG">
-                                    </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="formGroupExampleInput">JUMLAH_BARANG:</label>
-                                        <input type="text" class="form-control" id="formGroupExampleInput"
-                                            name="JUMLAH_BARANG">
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="formGroupExampleInput">JUMLAH_HARGA :</label>
-                                        <input type="text" class="form-control" id="formGroupExampleInput"
-                                            name="JUMLAH_HARGA">
-                                    </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="formGroupExampleInput">KETERANGAN :</label>
-                                        <input type="text" class="form-control" id="formGroupExampleInput"
-                                            name="KETERANGAN">
-                                    </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6 align-middle">
-                                        <button type="submit" name="insert_kib_a"
-                                            class="btn btn-primary">Simpan</button>
-                                        <button value="kembali" class="btn btn-secondary"><a
-                                                href="kib-a.php"></a></button>
-                                    </div>
-                                </div>
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+<?php
+} ?>
--- link/_usulan-penghapusan.php
+++ link/_usulan-penghapusan.php
@@ -4,14 +4,14 @@
       <div class="content">
         <div class="row">
           <div class="col-md-12">
-          <div class="card"> <button type="button" class="btn btn-outline-primary ml-2"> <a href="index.php?page=usulan-penghapusan-input">Input Data</a> </button>
-            <div class="card">
-              <div class="card-header" >
-                <p class="card-title text-center"><strong>DAFTAR USULAN BARANG YANG AKAN DIHAPUS</strong></p>
-              </div>
-              <div class="card-body" style="font-size:8pt;">                
-                <div class="row">
-                <table class="ml-4 mb-4" width="80%">
+            <div class="card"> <button type="button" class="btn btn-outline-primary ml-2"> <a href="index.php?page=usulan-penghapusan-input">Input Data</a> </button>
+              <div class="card">
+                <div class="card-header">
+                  <p class="card-title text-center"><strong>DAFTAR USULAN BARANG YANG AKAN DIHAPUS</strong></p>
+                </div>
+                <div class="card-body" style="font-size:8pt;">
+                  <div class="row">
+                    <table class="ml-4 mb-4" width="80%">
                       <tr>
                         <td width="13%">PROVINSI</td>
                         <td width="2%"> : </td>
@@ -37,65 +37,65 @@
                         <td> : </td>
                         <td> Sekretariat Dinas</td>
                       </tr>
-                      </table>
-                </div>              
-                <div class="table table-responsive">
-                  <table id="table_id" class="display">
-                    <thead class="text-primary">
-                      <tr>
-                        <th>No</th>
-                        <th>No. Kode Barang</th>
-                        <th>No. Register</th>
-                        <th>Nama Barang</th>
-                        <th>Merk / Tipe</th>
-                        <th>No. Sertifikat</th>
-                        <th>Bahan</th>
-                        <th>Cara Perolehan / Status Barang </th>
-                        <th>Tahun Perolehan</th>
-                        <th>Ukuran Barang/Konstruksi (P,SP,D)</th>
-                        <th>Keadaan Barang (B,KB,RB)</th>
-                        <th>Jumlah Barang</th>
-                        <th>Harga</th>
-                        <th>Keterangan/Tgl.Buku/Tahun Sensus</th>
-                        <th>Operasi</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                    <?php   
-                      // include connect udah di index
-                        
-                        $result = mysqli_query($connect,"SELECT * from USULAN_PENGHAPUSAN");
+                    </table>
+                  </div>
+                  <div class="table table-responsive">
+                    <table id="table_id" class="display">
+                      <thead class="text-primary">
+                        <tr>
+                          <th>No</th>
+                          <th>No. Kode Barang</th>
+                          <th>No. Register</th>
+                          <th>Nama Barang</th>
+                          <th>Merk / Tipe</th>
+                          <th>No. Sertifikat</th>
+                          <th>Bahan</th>
+                          <th>Cara Perolehan / Status Barang </th>
+                          <th>Tahun Perolehan</th>
+                          <th>Ukuran Barang/Konstruksi (P,SP,D)</th>
+                          <th>Keadaan Barang (B,KB,RB)</th>
+                          <th>Jumlah Barang</th>
+                          <th>Jumlah Harga</th>
+                          <th>Keterangan/Tgl.Buku/Tahun Sensus</th>
+                          <th>Operasi</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php
+                        // include connect udah di index
 
-                        $i = 1;    
-                        while($data = mysqli_fetch_array($result)){ ?>
-                                        <tr>
-                                        <td class="text-center"><?php echo $i;?></td>
-                                        <td class="text-center"><?php echo $data['NOMOR_KODE_BARANG'];?></td>
-                                        <td class="text-center"><?php echo $data['NOMOR_REGISTER'];?></td>
-                                        <td class="text-center"><?php echo $data['NAMA_BARANG'];?></td>
-                                        <td class="text-center"><?php echo $data['MERK_TIPE'];?></td>
-                                        <td class="text-center"><?php echo $data['NO_SERTIFIKAT'];?></td>
-                                        <td class="text-center"><?php echo $data['BAHAN'];?></td>
-                                        <td class="text-center"><?php echo $data['CARA_PEROLEHAN'];?></td>
-                                        <td class="text-center"><?php echo $data['TAHUN_PEROLEHAN'];?></td>
-                                        <td class="text-center"><?php echo $data['UKURAN_BARANG'];?></td>
-                                        <td class="text-center"><?php echo $data['KEADAAN_BARANG'];?></td>
-                                        <td class="text-center"><?php echo $data['JUMLAH_BARANG'];?></td>
-                                        <td class="text-center"><?php echo $data['JUMLAH_HARGA'];?></td>
-                                        <td class="text-center"><?php echo $data['KETERANGAN'];?></td>
-                                        <td>
-                                          <button type="button" class="btn-xs btn-outline-primary ml-2 mb-2"><a href="index.php?page=usulan-penghapusan-edit&kode=<?php echo $data['ID_USULAN'];?>" title="Edit" style="text-decoration:none;">Edit</a></button>
-                                          <button type="button" class="btn-xs btn-outline-danger ml-2"> <a href="link/crud.php?process=delete_usulan&kode=<?php echo $data['ID_USULAN'];?>">Delete</a> </button>
-                                        </td>                                        
-                                        </tr>  
-                                        
-                      <?php $i++;
-                      } ?>  
-                    </tbody>
-                  </table>
+                        $result = mysqli_query($connect, "SELECT * from USULAN_PENGHAPUSAN");
+
+                        $i = 1;
+                        while ($data = mysqli_fetch_array($result)) { ?>
+                          <tr>
+                            <td class="text-center"><?php echo $i; ?></td>
+                            <td class="text-center"><?php echo $data['NOMOR_KODE_BARANG']; ?></td>
+                            <td class="text-center"><?php echo $data['NOMOR_REGISTER']; ?></td>
+                            <td class="text-center"><?php echo $data['NAMA_BARANG']; ?></td>
+                            <td class="text-center"><?php echo $data['MERK_TIPE']; ?></td>
+                            <td class="text-center"><?php echo $data['NO_SERTIFIKAT']; ?></td>
+                            <td class="text-center"><?php echo $data['BAHAN']; ?></td>
+                            <td class="text-center"><?php echo $data['CARA_PEROLEHAN']; ?></td>
+                            <td class="text-center"><?php echo $data['TAHUN_PEROLEHAN']; ?></td>
+                            <td class="text-center"><?php echo $data['UKURAN_BARANG']; ?></td>
+                            <td class="text-center"><?php echo $data['KEADAAN_BARANG']; ?></td>
+                            <td class="text-center"><?php echo $data['JUMLAH_BARANG']; ?></td>
+                            <td class="text-center"><?php echo $data['JUMLAH_HARGA']; ?></td>
+                            <td class="text-center"><?php echo $data['KETERANGAN']; ?></td>
+                            <td>
+                              <button type="button" class="btn-xs btn-outline-primary ml-2 mb-2"><a href="index.php?page=usulan-penghapusan-edit&id=<?php echo $data['ID_USULAN']; ?>" title="Edit" style="text-decoration:none;">Edit</a></button>
+                              <button type="button" class="btn-xs btn-outline-danger ml-2"> <a href="link/crud.php?process=delete_usulan&id=<?php echo $data['ID_USULAN']; ?>">Delete</a> </button>
+                            </td>
+                          </tr>
+
+                          <?php $i++;
+                        } ?>
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
